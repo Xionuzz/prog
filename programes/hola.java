928eaa0ff969bb8104ceec4236fa6fe8d10e841e
Kalash <you@example.com>
2019-09-29 18:07:43 +0200

    Remodelada la estructura del repositorio

---
+++ programes/hola.java
@@ -0,0 +1,22 @@
+public class hola {
+
+    public static void main (String args []) {
+
+	String nombreCompleto = "RCP";
+	System.out.println("Hola " + nombreCompleto + " me alegra verte de nuevo");
+	double d = Math.random();
+	System.out.println(d);
+	/* Esto es un comentario */
+	// Esto también
+	int x = 1;
+	while (x < 10) {
+	    x = x + 1;
+	    System.out.println(x);
+	}
+       
+	for (int y = 0; y < 10; y = y + 1) {
+	    System.out.println("El valor de y es " + y);
+	}
+
+    }
+}
---
+++ programes/numero.java
@@ -0,0 +1,10 @@
+public class numero {
+
+    public static void main (String args []) {
+
+	int x;
+	x = 3 * 17;
+	System.out.print("x es" + x);
+	
+    }
+}
