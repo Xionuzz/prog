031717270cc017f8460761639f2af9effecc9bc1
OldKalash7 <kalash7@protonmail.com>
2019-10-30 13:52:23 +0100

    arreglado color

--- NumerosColor.java
+++ NumerosColor.java
@@ -4,12 +4,14 @@ public class NumerosColor {
     public static void main(String Args[]) {
 
 		String s = "";
-		
+		int color = 30; // Controla el color del output.
+
 		/*Imprime formateado en pantalla los números del 1 al 9*/
 		
 		for(int i = 1; i <= 9; i++){
 			s = s + i;
-			System.out.printf("\u001B[1;30;40m%9s\n",s);
+			System.out.printf("\u001B["+color+"m%9s\n",s);
+			color++;
 		}
     }
 }
