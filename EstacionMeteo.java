e51ffab841de26e2e8c9e0dfbed48030414b4b84
Kalash <you@example.com>
2019-11-04 23:31:36 +0100

    Modificado estacion

--- EstacionMeteo.java
+++ EstacionMeteo.java
@@ -5,6 +5,7 @@ public class EstacionMeteo {
 
         String nombreAplicacion = "** ESTACIÓN METEREOLÓGICA **";
         String nombreCiudad = "";
+	String tab = "";
         int dia = 0;
         int mes = 0;
         int año = 0;
@@ -12,26 +13,24 @@ public class EstacionMeteo {
         int minuto = 0;
         int segundo = 0;
         int velocidadViento = 0;
-        double temperatura = 0;
-        double presionAtmos = 0;
         int probLluvia = 0;
         int uvi = 0;
-
-        String tab = "";
+	double temperatura = 0;
+        double presionAtmos = 0;
 
         Scanner scan = new Scanner();
 
         System.out.println(nombreAplicacion);
         System.out.println("Introduce los datos por favor");
 
-        do{
+        do {
             System.out.print("Nombre ciudad:");
             nombreCiudad = scan.nextLine();
             scan.nextLine();
 
-        }while(nombreCiudad = "");
+        } while(nombreCiudad = "");
 
-        do{
+        do {
             System.out.print("Introduce día:");
             dia = scan.nextInt();
             scan.nextLine();
@@ -41,9 +40,9 @@ public class EstacionMeteo {
             System.out.print("Introduce año:");
             año = scan.nextInt();
             scan.nextLine();
-        }while(dia == 0 || mes == 0 || año == 0);
+        } while(dia == 0 || mes == 0 || año == 0);
 
-        do{
+        do {
             System.out.print("Introduce hora:");
             hora = scan.nextInt();
             scan.nextLine();
@@ -53,84 +52,84 @@ public class EstacionMeteo {
             System.out.print("Introduce segundo:");
             segundo = scan.nextInt();
             scan.nextLine();
-        }while(hora < 24 || minuto < 60 || segundo < 60);
+        } while(hora >= 24 || minuto >= 60 || segundo >= 60);
 
-        do{
+        do {
             System.out.print("Introduce la velocidad del viento:");
             velocidadViento = scan.nextInt();
             scan.nextLine();
 
-        }while(velocidadViento < 0);
+        } while(velocidadViento < 0);
 
-        do{
+        do {
             System.out.print("Introduce la temperatura (En grados CELSIUS):");
             temperatura = scan.nextDouble();
             scan.nextLine();
-        }while(temperatura = null);
+        } while(temperatura == null);
 
-        do{
+        do {
             System.out.print("Introduce presión atmosférica:");
             presionAtmos = scan.nextDouble();
             scan.nextLine();
 
-        }while(presionAtmos = 0);
+        } while(presionAtmos == null);
 
-        do{
+        do {
             System.out.print("Introduce la probabilidad de lluvia (0-100):");
             probLluvia = scan.nextInt();
             scan.nextLine();
 
-        }while(probLluvia < 0 || probLluvia > 100);
+        } while(probLluvia < 0 || probLluvia > 100);
 
-        do{
+        do {
             System.out.print("Introduce índice de radiación ultravioleta:");
             uvi = scan.nextInt();
             scan.nextLine();
 
-        }while(uvi = 0);
+        } while(uvi < 0);
 
         // Imprimir formateado en pantalla.
 
         System.out.printf("\u001B[1;37;44m" + "%s\n",nombreAplicacion);
         System.out.printf("Ciudad:" + "%s%s",tab,nombreCiudad);
-        System.out.printf("%s%2d/%2d/%4d",tab, dia, mes, año);
-        System.out.printf("%s%02d:%02d:%02d",tab, hora, minuto, segundo);
+        System.out.printf("%10s" + "%s%2d/%2d/%4d",tab, dia, mes, año);
+        System.out.printf("%10s" + "%02d:%02d:%02d",tab, hora, minuto, segundo);
 
         if (velocidadViento < 30) {
-            System.out.printf("%10s" + "\u001B[1;37;44m",tab, velocidadViento);
+            System.out.printf("%10s" + "\u001B[1;32;40m" + "%2d",tab, velocidadViento);
         }
 
         else if (velocidadViento < 61){
-            System.out.printf("");
+            System.out.printf("%10s" + "\u001B[1;33;40m" + "%2d",tab, velocidadViento);
         }
         else 
-            System.out.printf("");
+	    System.out.printf("%10s" + "\u001B[1;31;40m" + "%2d",tab, velocidadViento);
 
         switch(temperatura){
             case 0:
             case 35:
-                System.out.printf("%10d" + "%",probLluvia);
+                System.out.printf("%10d" + "%2d",tab, probLluvia);
                 break;
             case 70: 
-                System.out.printf("%10d" + "%",probLluvia);
+                System.out.printf("%10d" + "%2d",tab, probLluvia);
                 break;
             case 100:
-                System.out.printf("%10d" + "%",probLluvia);
+                System.out.printf("%10d" + "%2d",tab, probLluvia);
                 break;
         }
 
-        System.out.printf("%10.1f",presionAtmos);
+        System.out.printf("%10s""%10.1f",tab, presionAtmos);
         
         switch(probLluvia){
             case 0:
             case 35:
-                System.out.printf("%10d" + "%",probLluvia);
+                System.out.printf("%10d" + "\u001B[1;32;40m" + "%2d",probLluvia);
                 break;
             case 70: 
-                System.out.printf("%10d" + "%",probLluvia);
+                System.out.printf("%10d" + "\u001B[1;33;40m" + "%2d",probLluvia);
                 break;
             case 100:
-                System.out.printf("%10d" + "%",probLluvia);
+                System.out.printf("%10d" + "\u001B[1;31;40m" + "%2d",probLluvia);
                 break;
         }
 
