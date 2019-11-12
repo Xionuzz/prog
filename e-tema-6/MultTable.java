431f42c94bc0823e4f77825e0f97228666fee150
rcp <rcppedreguer@gmail.com>
2019-11-12 19:15:49 +0100

    correcciones

--- e-tema-6/MultTable.java
+++ e-tema-6/MultTable.java
@@ -9,24 +9,34 @@ public class MultTable {
     public static void main(String Args[]) {
 
         int number = 0;
+        boolean multiplication = true;
 
         Scanner scan = new Scanner(System.in);
-
-        System.out.print("Introduce a number(interger): ");
-        number = scan.nextInt();
-        scan.nextLine();
-        multiplication(number);
+        
+        while(multiplication){
+            System.out.print("Introduce a number(integer): ");
+            number = scan.nextInt();
+            scan.nextLine();
+            if (number < 0 ){
+                multiplication = false;
+            }
+            multiplication(number);
+        }
     }
 
     
     /**
-     * Outputs to screen character a numberTimes
-     * @param character
-     * @param numberTimes
+     * Outputs to screen the multiplication table of a given integer
+     * @param number an integer
+     * @return void
      */
 
 
     public static void multiplication(int number){
+
+        if (number < 0){
+            return;
+        }
 
         for(int x = 1; x <= 10; x++){
             System.out.println(number + "x" + x + "=" + number * x);
