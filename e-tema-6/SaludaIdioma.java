c7212f0782377c042caa317c280dd1bd75ac4399
rcp <rcppedreguer@gmail.com>
2019-11-13 19:12:44 +0100

    correcciones

---
+++ e-tema-6/SaludaIdioma.java
@@ -0,0 +1,23 @@
+/**
+ * Ejercicio 13
+ * @author Rafael Carrió
+ * Escribe un mensaje de saludo en el idioma elegido.
+ */
+
+
+import java.util.Scanner;
+
+ public class SaludaIdioma {
+
+    public static void main(String Args[]){
+
+        System.out.print("Introduce tu nombre: ");
+
+    }
+
+    public void saludoPersonalizado(){
+
+
+    }
+
+}
