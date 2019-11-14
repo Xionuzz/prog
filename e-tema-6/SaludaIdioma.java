989be20944f1c034938150c244175a89ace5f14d
Kalash <you@example.com>
2019-11-14 10:11:37 +0100

    correcciones

--- e-tema-6/SaludaIdioma.java
+++ e-tema-6/SaludaIdioma.java
@@ -10,12 +10,40 @@ import java.util.Scanner;
  public class SaludaIdioma {
 
     public static void main(String Args[]){
+        
+        Scanner scan = new Scanner(System.in);
 
+        boolean validado = false;
+        int codigo = 0;
+        String nombre = "";
         System.out.print("Introduce tu nombre: ");
+        nombre = scan.nextLine();
 
+        do{
+            System.out.print("Introduce el idioma (1-ENG 2-ESP 3-CAT)");
+            codigo = scan.nextInt();
+            scan.nextLine();
+            if (codigo > 0 && codigo < 4){
+                validado = true;
+            }
+        } while(!validado);
+
+       saludoPersonalizado(nombre, codigo);
     }
 
-    public void saludoPersonalizado(){
+    public void saludoPersonalizado(String test, int codigo){
+
+        switch(codigo){
+            case 1:
+                System.out.println();
+                break;
+            case 2:
+                System.out.println();
+                break;
+            case 3:
+                System.out.println();
+                break;
+        }
 
 
     }
