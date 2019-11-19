86e6537db5f7b7d66fd3f0c30f0b8fabae62d24f
OldKalash7 <kalash7@protonmail.com>
2019-11-19 12:57:29 +0100

    correcciones

--- e-tema-6/SecuenciaCeros.java
+++ e-tema-6/SecuenciaCeros.java
@@ -24,14 +24,15 @@ public class SecuenciaCeros {
 
         boolean ejecutarBucle = false;
 
-        for(int i = 0; i <= 200; i++){
-            System.out.printf("%d-%d-%d-%d\n",numeroUno,numeroDos,numeroTres,numeroCuatro);
-
+        for(int i = 0; i <= 9; i++){
+            System.out.printf("%d-",numeroUno);
+            numeroUno++;
+          
             //if(numeroUno == 5 || numeroDos == 5 || numeroTres == 5 || numeroCuatro == 5){
               //  letra();
             //}
 
-            if(numeroCuatro < 9){
+           /* if(numeroCuatro < 9){
                 numeroCuatro++;
             } else if (numeroTres < 9){
                 numeroTres++;
@@ -44,9 +45,19 @@ public class SecuenciaCeros {
             } else if (numeroUno < 9){
                 numeroUno++;
             }
-
+            */
             //System.out.printf("%d" + "-" + "%d" + "-" + "%d" + "-" + "%d",numeroUno,numeroDos,numeroTres,numeroCuatro);
+            for(int j = 0; j <= 9; j++){
+                System.out.printf("%d-",numeroUno);
+                numeroDos++;
+            }
+       
+       
         }
+
+        
+        
+        System.out.println("");
     }
 
     public static String letra(){
